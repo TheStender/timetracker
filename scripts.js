0b9c930429ab5223604c033b002bbb7d225bcfd5
Anthony Stender <anthony.m.stender@gmail.com>
2019-03-29 13:18:56 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
 
 	$("#submitData").click(function () {
-		var activity = $('#activity').val();
-		var totalTime = $('#totalTime').val();
+		var activitytype = $('#activityType').val();
+		var activityname = $('#activityName').val();
+		var currenttime = $('#currentTime').val();
+		var totaltime = $('#totalTime').val();
 		
 				if (confirm("Do you want to submit this activity?")) {
 					$.post("http://localhost:59584/api/tracker", {
-						data: { activity: activity, time: totalTime }
+						data: { activitytype: activitytype, 
+								activityname: activityname,
+								currenttime: currenttime,
+								totaltime: totaltime}
 					}, function (data, status) {
 						alert(data.message);
 							})
